330d4598b7634428711af768e9ee35feb7a7172e
Brett Haines <bhaines3@gatech.edu>
2019-10-11 23:31:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## My Shadow's Keeper Readme
+
+The light is calling you, but to reach it you'll have to work together with your shadow.
+
+This game was created for the [14th Godot Wild Jam](https://itch.io/jam/godot-wild-jam-14).  It is based on the theme "shadows".
+
+All code and assets are free to use under the MIT license.
---
+++ scripts/character.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+# Constants
+const MOVE_SPEED : int = 400
+const JUMP_SPEED : int = -600
+const GRAVITY : Vector2 = Vector2(0, 980)
+
+# Scene elements
+onready var sprite = $self_sprite
+onready var shadow = $shadow_sprite
+
+# State variables
+var velocity : Vector2 = Vector2()
+
+
+func _physics_process(delta : float):
+	
+	# Handle gravity
+	velocity += delta * GRAVITY
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	# Handle player input
+	var direction : int = 0
+	if Input.is_action_pressed("move_right"):
+		direction += MOVE_SPEED
+	if Input.is_action_pressed("move_left"):
+		direction -= MOVE_SPEED
+	velocity.x = lerp(velocity.x, direction, 0.1)
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_SPEED
+	
+	move_shadow()
+
+func move_shadow():
+	shadow.global_position.x = sprite.global_position.x
+	shadow.global_position.y = sprite.global_position.y * -1
