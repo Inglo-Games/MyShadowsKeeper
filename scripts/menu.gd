603f83f36e550a887f3d027e6a9337f2f1de8499
Brett Haines <bhaines3@gatech.edu>
2019-10-16 19:22:12 -0400

    Adding function for credits button

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -6,5 +6,8 @@ func _on_start_pressed():
 func _on_tut_pressed():
 	get_tree().change_scene("res://scenes/tutorial.tscn")
 
+func _on_attr_pressed():
+	get_tree().change_scene("res://scenes/credits.tscn")
+
 func _on_quit_pressed():
 	get_tree().quit()
